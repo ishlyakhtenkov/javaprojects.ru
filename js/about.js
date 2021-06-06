2a7e14dfe508f101e236fbf1b47520e7245567c9
igar15 <igar15@yandex.ru>
2021-06-06 09:30:41 +0300

    add includes to project pages

--- js/about.js
+++ js/about.js
@@ -2,6 +2,10 @@ function rotateIcon() {
     $('#collapsed-chevron').toggleClass('fa-rotate-180');
 }
 
+function closeImgModal() {
+    modal.style.display = "none";
+}
+
 function goTop() {
     document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
