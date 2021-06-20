b52cbbb1ae0f0ca0f4de055fe276bb9f4f1f5cfa
igar15 <igar15@yandex.ru>
2021-06-20 16:31:47 +0300

    change html code

--- js/include.js
+++ js/include.js
@@ -1,4 +1,4 @@
-    $(function(){
+    $(function(){		
       $("#footerContent").load("fragments/footer.html"); 
       $("#navbarContent").load("fragments/navbar.html"); 
       $("#aboutContent").load("fragments/about.html"); 
