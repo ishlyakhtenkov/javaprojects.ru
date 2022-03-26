65d7aecf93f091e844f0ff04a9375b97d2225c12
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-03-26 17:41:24 +0300

    update site style

--- js/about.js
+++ js/about.js
@@ -2,9 +2,6 @@ function rotateIcon() {
     $('#collapsed-chevron').toggleClass('fa-rotate-180');
 }
 
-function closeImgModal() {
-    modal.style.display = "none";
-}
 
 function goTop() {
     document.body.scrollTop = 0;
