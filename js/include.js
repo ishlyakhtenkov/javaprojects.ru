a4656f13b5e5eb821a463f4210dc4fc984c4efc1
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-07-31 20:23:54 +0300

    change site design

--- js/include.js
+++ js/include.js
@@ -1,5 +1,4 @@
     $(function(){		
       $("#footerContent").load("fragments/footer.html"); 
       $("#navbarContent").load("fragments/navbar.html"); 
-      $("#aboutContent").load("fragments/about.html"); 
     });
