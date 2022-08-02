335d82b2927820ec07be166c21b044e6e94d7cf7
Igor Shlyakhtenkov <ishlyakhtenkov@yandex.ru>
2022-08-02 11:17:08 +0300

    refactor

--- js/about.js
+++
@@ -1,9 +0,0 @@
-function rotateIcon() {
-    $('#collapsed-chevron').toggleClass('fa-rotate-180');
-}
-
-
-function goTop() {
-    document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
-}
